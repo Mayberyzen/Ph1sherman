e1baf23ef8a8da6f8c2762649a0545d8858de3f4
ryzen_GG <mahapatrachetan@gmail.com>
2025-03-12 20:31:23 +0530

    content.js

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 console.log("Ph1sherman content script loaded.");
 
-// Example functionality (modify if needed)
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "check_url") {
         console.log("Checking URL:", message.url);
